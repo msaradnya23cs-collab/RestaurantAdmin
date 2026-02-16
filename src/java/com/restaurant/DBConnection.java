90392b177c5fc8dbfe44eff533fcd628707b6a46
msaradnya23cs-collab <msaradnya23cs@student.mes.ac.in>
2026-02-16 11:14:22 +0530

    Update DBConnection to PostgreSQL and deploy WAR

--- src/java/com/restaurant/DBConnection.java
+++ src/java/com/restaurant/DBConnection.java
@@ -1,7 +1,4 @@
-
-    
-    
-    package com.restaurant;
+package com.restaurant;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
@@ -13,17 +10,16 @@ public class DBConnection {
         try {
             Class.forName("org.postgresql.Driver");
 
-            // Read Render environment variables
-             String URL = System.getenv("jdbc:postgresql://dpg-d698p26mcj7s738kv76g-a.oregon-postgres.render.com:5432/restaurantdb_9uyr?sslmode=require");      // Render PostgreSQL JDBC URL
-            String USER = System.getenv("admin");    // Render PostgreSQL user
-            String PASSWORD = System.getenv("JJmR5tx2uEw71oUZdwQKjQApcql58ulX"); // Render PostgreSQL password
+            // Use your Render PostgreSQL credentials directly
+            String URL = "jdbc:postgresql://dpg-d698p26mcj7s738kv76g-a.oregon-postgres.render.com:5432/restaurantdb_9uyr?sslmode=require";
+            String USER = "admin";
+            String PASSWORD = "JJmR5tx2uEw71oUZdwQKjQApcql58ulX";
 
             con = DriverManager.getConnection(URL, USER, PASSWORD);
+
         } catch (Exception e) {
             e.printStackTrace();
         }
         return con;
     }
 }
-
-
